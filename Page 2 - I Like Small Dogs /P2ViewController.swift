c4d217f3840c5bf6a923750d867a8a8c80525faf
Bobby Tagget <bobby.tagget@gmail.com>
2018-04-12 15:20:17 +1000

    Added view controllers for each page

---
+++ Page 2 - I Like Small Dogs /P2ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  P2ViewController.swift
+//  I Like Dogs
+//
+//  Created by Bobby Tagget on 12/4/18.
+//  Copyright © 2018 Bobby Tagget. All rights reserved.
+//
+
+import UIKit
+
+class P2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
